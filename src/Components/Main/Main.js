d76687acbe1abf2877aa34aeeacfbdb27738f824
longpatrick <long.patrick1997@gmail.com>
2021-06-01 16:30:30 -0500

    add Main component

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Main.module.css';
+
+const Main = () => {
+  return (
+    <div className={styles.Main}>
+      <p>See if you can complete all of the Coding_Challenges!</p>
+    </div>
+  )
+};
+
+export default Main;
