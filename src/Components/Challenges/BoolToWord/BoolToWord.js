196e3f411dc8a2432c2bcbced18e5f37194f3b08
longpatrick <long.patrick1997@gmail.com>
2021-06-02 16:27:44 -0500

    wireframe challenge, change and submit functions

--- src/Components/Challenges/BoolToWord/BoolToWord.js
+++ src/Components/Challenges/BoolToWord/BoolToWord.js
@@ -1,12 +1,29 @@
 import styles from './BoolToWord.module.css';
+import { useState } from 'react';
 
 const BoolToWord = () => {
+
+    const isBoolean = (str) => {
+
+    }
+
+    const onChange = () => {
+
+    }
+
+    const onSubmit = event => {
+        event.preventDefault();
+
+    }
     return (
         <div className={styles.BoolToWord}>
             <h2 className="challenge-title">BoolToWord</h2>
-            <form className="challenge-form">
+            <p className="challenge-description">Input a boolean (true or false) value and return either "Yes" for true or "No" for false</p>
+            <form className="challenge-form" onSubmit={onSubmit}>
                 <input type="text" name="challenge-input" id="bool-to-word" />
-                <button type="submit" className="btn btn-success">Submit</button>
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
             </form>
         </div>
     )
