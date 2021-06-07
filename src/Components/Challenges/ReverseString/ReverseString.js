7003b7fea72d836c30b2b350a9088986628c8076
longpatrick <long.patrick1997@gmail.com>
2021-06-07 14:52:51 -0500

    add additional component for reverse string

---
+++ src/Components/Challenges/ReverseString/ReverseString.js
@@ -0,0 +1,50 @@
+import styles from './ReverseString.module.css';
+import { useState } from 'react';
+
+const reverseString = () => {
+
+    const [state, setState] = useState({
+        input: '',
+        result: ''
+    })
+
+    const reverseStringFunction = str => {
+
+    }
+
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = isBoolean(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }))
+    }
+
+    return (
+        <div className="challenge-container">
+            <h2 className="challenge-title">Reverse a string</h2>
+            <p className="challenge-description">Input a boolean (true or false) value and return either "Yes" for true or "No" for false</p>
+            <form className="challenge-form" onSubmit={handleSubmit}>
+                <input type="text" value={state.input} onChange={handleChange} name="input" id="bool-to-word" className="form-control challenge-input" placeholder='"true" or "false"'
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
+    )
+}
+
+export default reverseString;
+
