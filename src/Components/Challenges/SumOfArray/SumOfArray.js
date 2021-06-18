7f538a15f327abc15d7f28f0019eafcc724d5bc9
longpatrick <long.patrick1997@gmail.com>
2021-06-18 13:19:16 -0500

    start working towards completing sumofArray component

--- src/Components/Challenges/SumOfArray/SumOfArray.js
+++ src/Components/Challenges/SumOfArray/SumOfArray.js
@@ -1,12 +1,38 @@
 import { useState } from 'react'; 
 
 const SumOfArray = () => {
+
+    const [state, setState] = useState({
+        input: '',
+        result: ''
+    })
+
+    const sumOfArrayFunction = str => {
+        console.log(str);
+    }
+
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = sumOfArrayFunction(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }))
+    }
+    
     return (
         <div className="challenge-container">
-            <h2 className="challenge-title">Reverse a string</h2>
-            <p className="challenge-description">Input a string to reverse</p>
+            <h2 className="challenge-title">Find the sum of an array</h2>
+            <p className="challenge-description">Input a series of numbers to reduce in "[number, number, number]" format</p>
             <form className="challenge-form" onSubmit={handleSubmit}>
-                <input type="text" value={state.input} onChange={handleChange} name="input" id="reverse-string" className="form-control challenge-input" placeholder="Input string here"
+                <input type="text" value={state.input} onChange={handleChange} name="input" id="reverse-string" className="form-control challenge-input" placeholder="Input numbers here"
                 />
                 <button type="submit" className="btn btn-success">
                     Result
