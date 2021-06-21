30aa3734e7af3f6642991d333fb8dfbdc2bf00bd
longpatrick <long.patrick1997@gmail.com>
2021-06-21 12:41:46 -0500

    add handleChange and handleSubmit functions

--- src/Components/Challenges/NumberofMilliseconds/NumberofMilliseconds.js
+++ src/Components/Challenges/NumberofMilliseconds/NumberofMilliseconds.js
@@ -1,6 +1,33 @@
 import { useState, useEffect } from "react";
 
 const NumberofMilliseconds = () => {
+
+    const [state, setState] = useState({
+        input: '',
+        result: ''
+    })
+
+    const numberOfMillisecondsFunction = str => {
+        return str.split('').reverse().join('');
+    }
+
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = numberOfMillisecondsFunction(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }))
+    }
+
+
     return (
         <div className="challenge-container">
             <h2 className="challenge-title">Reverse a string</h2>
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -17,6 +17,7 @@ const Main = () => {
       <NumToReverseArray />
       <IsSquare />
       <SumOfArray />
+      <NumberofMilliseconds />
     </div>
   )
 };
