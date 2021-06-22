218bf43390f74d0ee057a805c2a891521043e2ae
longpatrick <long.patrick1997@gmail.com>
2021-06-22 16:24:31 -0500

    implement useState react hook and write handleChange and handleSubmit functions

--- src/Components/Challenges/OddOrEven/OddOrEven.js
+++ src/Components/Challenges/OddOrEven/OddOrEven.js
@@ -2,6 +2,26 @@ import { useState } from 'react';
 
 const OddOrEven = () => {
 
+    const [state, setState] = useState({
+        input: '',
+        result: ''
+    })
+
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = numberOfMillisecondsFunction(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }))
+    }
 
     return (
         <div className="challenge-container">
