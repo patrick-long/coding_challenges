c4cf19a8668e409e799f2f9ea8ba84d2c5271684
longpatrick <long.patrick1997@gmail.com>
2021-06-23 18:23:46 -0500

    complete display div in NearestFive component

--- src/Components/Challenges/NearestFive/NearestFive.js
+++ src/Components/Challenges/NearestFive/NearestFive.js
@@ -2,7 +2,20 @@ import {useState} from 'react';
 
 const NearestFive = () => {
     return (
-        <div></div>
+        <div className="challenge-container">
+            <h2 className="challenge-title">Nearest Multiple of 5</h2>
+            <p className="challenge-description">Input a number to round up to the nearest multiple of 5</p>
+            <form className="challenge-form" onSubmit={handleSubmit}>
+                <input type="text" value={state.input} onChange={handleChange} name="input" className="form-control challenge-input" placeholder="Input number here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
     )
 }
 
