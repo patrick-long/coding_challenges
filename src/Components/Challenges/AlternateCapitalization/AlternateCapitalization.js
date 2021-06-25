ec5c1ca5c261ce8f8b4e1337a6c2d2757c88b487
longpatrick <long.patrick1997@gmail.com>
2021-06-25 13:07:18 -0500

    use useState hook properly

--- src/Components/Challenges/AlternateCapitalization/AlternateCapitalization.js
+++ src/Components/Challenges/AlternateCapitalization/AlternateCapitalization.js
@@ -2,31 +2,36 @@ import {useState} from 'react';
 
 const AlternateCapitalization = () => {
 
-    const alternateCapitalizationFunction = (s) => {
-        const splitString = s.split('');
-        const result1 = [];
-        const result2 = [];
-        for (let i = 0; i < s.length; i++) {
-          if (i % 2 == 0) {
-            const capitalized = splitString[i].toUpperCase();
-            result1.push(capitalized);
-          } else {
-            result1.push(splitString[i])
-          }
+  const [state, setState] = useState({
+    input: '',
+    result: ''
+  });
+
+  const alternateCapitalizationFunction = (s) => {
+      const splitString = s.split('');
+      const result1 = [];
+      const result2 = [];
+      for (let i = 0; i < s.length; i++) {
+        if (i % 2 == 0) {
+          const capitalized = splitString[i].toUpperCase();
+          result1.push(capitalized);
+        } else {
+          result1.push(splitString[i])
         }
-        
-        
-        for (let i = 0; i < s.length; i++) {
-          if (i % 2 == 1) {
-            const capitalized = splitString[i].toUpperCase();
-            result2.push(capitalized);
-          } else {
-            result2.push(splitString[i])
-          }
+      }
+      
+      
+      for (let i = 0; i < s.length; i++) {
+        if (i % 2 == 1) {
+          const capitalized = splitString[i].toUpperCase();
+          result2.push(capitalized);
+        } else {
+          result2.push(splitString[i])
         }
-        
-        return [result1.join(''), result2.join('')];
-      };
+      }
+      
+      return [result1.join(''), result2.join('')];
+    };
 
 
     return (
