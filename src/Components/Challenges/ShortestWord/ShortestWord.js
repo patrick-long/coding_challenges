0e0a7b651b477baa86ad9914ca7c463548d4edba
longpatrick <long.patrick1997@gmail.com>
2021-07-01 16:10:03 -0500

    create display HTML for ShortestWord component

--- src/Components/Challenges/ShortestWord/ShortestWord.js
+++ src/Components/Challenges/ShortestWord/ShortestWord.js
@@ -4,7 +4,28 @@ const ShortestWord = () => {
 
 
     return (
-        <div></div>
+        <div className="challenge-container">
+            <h2 className="challenge-title">How many vowels in a string?</h2>
+            <p className="challenge-description">Input a string to return how many vowels there are in the string</p>
+            <form 
+                className="challenge-form" 
+                onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={state.input} 
+                    onChange={handleChange} 
+                    name="input" 
+                    className="form-control challenge-input" 
+                    placeholder="Input string here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
     )
 }
 
