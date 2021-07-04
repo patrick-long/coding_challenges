5d195e05921a4ebf78333ca1ecf57c286ed16cb9
longpatrick <long.patrick1997@gmail.com>
2021-07-04 11:29:41 -0500

    add footer and coding challenge links to application

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
         <h3>Welcome to Coding_Challenges!</h3>
       </header>
       <Main />
+      <footer>
+        <h4>Here are some of the most popular coding challenge websites to help you learn algorithms, data structures and other fun coding stuff. Enjoy!</h4>
+        <div className="coding-links">
+          <a href="https://www.codewars.com">CodeWars</a>  
+          <a href="https://www.edabit.com">Edabit</a>
+          <a href="https://www.hackerrank.com">HackerRank</a>
+        </div>
+      </footer>
     </div>
   );
 }
