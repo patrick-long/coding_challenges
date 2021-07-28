64d4575bfc1064a9239682d1182f5275edea414d
longpatrick <long.patrick1997@gmail.com>
2021-07-28 12:27:37 -0500

    write useState, handleChange and handleSubmit functions

--- src/Components/Challenges/SumDigits/SumDigits.js
+++ src/Components/Challenges/SumDigits/SumDigits.js
@@ -1,6 +1,36 @@
 import { useState } from 'react';
 
 const SumDigits = () => {
+
+    const [state, setState] = useState({
+        input: '',
+        result: ''
+    });
+    
+    const shortestWordFunction = (str) => {
+        const wordsArray = str.split(' ');
+        const lengthArray = wordsArray.map(word => word.length);
+        const shortestWordIndex = lengthArray.indexOf(Math.min(...lengthArray));
+        return wordsArray[shortestWordIndex];
+    };
+
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = shortestWordFunction(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }))
+    }
+
+    
     return (
         <div></div>
     )
