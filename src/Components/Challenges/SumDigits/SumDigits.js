543f76f65dcbc420f74f39cab195f962f1d776e4
longpatrick <long.patrick1997@gmail.com>
2021-07-28 12:32:15 -0500

    create display of functional component SumDigits

--- src/Components/Challenges/SumDigits/SumDigits.js
+++ src/Components/Challenges/SumDigits/SumDigits.js
@@ -42,7 +42,28 @@ const SumDigits = () => {
 
 
     return (
-        <div></div>
+        <div className="challenge-container">
+            <h2 className="challenge-title">Find sum of a number's digits</h2>
+            <p className="challenge-description">Input a number to return the sum of its digits</p>
+            <form 
+                className="challenge-form" 
+                onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={state.input} 
+                    onChange={handleChange} 
+                    name="input" 
+                    className="form-control challenge-input" 
+                    placeholder="Input number here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
     )
 }
 
