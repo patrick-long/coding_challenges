67785cdc88ab2b47cd2022bada4625e346500054
longpatrick <long.patrick1997@gmail.com>
2021-07-29 11:34:35 -0500

    write handleChange and handleSubmit functions

--- src/Components/Challenges/Arithmetic/Arithmetic.js
+++ src/Components/Challenges/Arithmetic/Arithmetic.js
@@ -2,6 +2,21 @@ import { useState } from 'react';
 
 const Arithmetic = () => {
 
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = arithmeticFunction(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }))
+    }
 
     return (
         <div></div>
