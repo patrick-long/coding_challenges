036aa27130101632ba0c199b62ffd4cf73279ede
longpatrick <long.patrick1997@gmail.com>
2021-07-29 11:36:28 -0500

    write arithmeticFunction

--- src/Components/Challenges/Arithmetic/Arithmetic.js
+++ src/Components/Challenges/Arithmetic/Arithmetic.js
@@ -2,6 +2,20 @@ import { useState } from 'react';
 
 const Arithmetic = () => {
 
+    const arithmeticFunction = (a, b, operator) => {
+        if (operator === 'add') {
+            return a + b
+        } else if (operator === 'subtract') {
+            return a - b
+        } else if (operator === 'multiply') {
+            return a * b
+        } else if (operator === 'divide') {
+            return a / b
+        } else {
+            return 'Please try "add", "subtract", "multiply" or "divide" for the operator.'
+        }
+    };
+
     const handleChange = e => {
         setState(prevState => ({
             ...prevState,
