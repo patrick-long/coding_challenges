d1dee0cba366be93e1074975309c0768a7d8ad8f
longpatrick <long.patrick1997@gmail.com>
2021-07-30 11:20:09 -0500

    write isIsogramFunction

--- src/Components/Challenges/IsIsogram/IsIsogram.js
+++ src/Components/Challenges/IsIsogram/IsIsogram.js
@@ -7,6 +7,18 @@ const IsIsogram = () => {
         result: ''
     });
 
+    const isIsogramFunction = (str) => {
+        const newStr = str.toLowerCase();
+        for (let i = 0; i < newStr.length; i++) {
+          for (let j = i + 1; j < newStr.length; j++) {
+            if (newStr[i] === newStr[j]) {
+              return false
+            } 
+          }
+        }
+        return true
+    }
+
     const handleChange = e => {
         setState(prevState => ({
             ...prevState,
