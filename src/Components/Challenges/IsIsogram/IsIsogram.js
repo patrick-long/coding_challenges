2a737f5efa6dcdc101edd0ac8f17fdcba717b28d
longpatrick <long.patrick1997@gmail.com>
2021-07-30 11:24:33 -0500

    write display of functional component IsIsogram

--- src/Components/Challenges/IsIsogram/IsIsogram.js
+++ src/Components/Challenges/IsIsogram/IsIsogram.js
@@ -36,7 +36,28 @@ const IsIsogram = () => {
     }
 
     return (
-        <div></div>
+        <div className="challenge-container">
+            <h2 className="challenge-title">Is a string an isogram?</h2>
+            <p className="challenge-description">Input a string to find out if it's an isogram (no repeat letters)</p>
+            <form 
+                className="challenge-form" 
+                onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={state.input} 
+                    onChange={handleChange} 
+                    name="input" 
+                    className="form-control challenge-input" 
+                    placeholder="Input string here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
     )
 };
 
