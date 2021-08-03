0979066956c8fe757776e43a86ca46fe896dc390
longpatrick <long.patrick1997@gmail.com>
2021-08-03 17:50:16 -0500

    write rest of functional component CamelCasing

--- src/Components/Challenges/CamelCasing/CamelCasing.js
+++ src/Components/Challenges/CamelCasing/CamelCasing.js
@@ -25,7 +25,28 @@ const CamelCasing = () => {
 
 
     return (
-        <div></div>
+        <div className="challenge-container">
+            <h2 className="challenge-title">Alter camelCasedStrings!</h2>
+            <p className="challenge-description">Input a string add a space between all camelCased words (eg. "thisIsAString")</p>
+            <form 
+                className="challenge-form" 
+                onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={state.input} 
+                    onChange={handleChange} 
+                    name="input" 
+                    className="form-control challenge-input" 
+                    placeholder="Input string here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
     )
 };
 
