0e5b50acf7049b809f0875aa47520585f1d31e73
longpatrick <long.patrick1997@gmail.com>
2021-08-03 17:48:09 -0500

    write handleChange and handleSubmit functions

--- src/Components/Challenges/CamelCasing/CamelCasing.js
+++ src/Components/Challenges/CamelCasing/CamelCasing.js
@@ -7,7 +7,23 @@ const CamelCasing = () => {
         result: ''
     });
 
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
     
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = camelCasingFunction(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }))
+    }
+
+
     return (
         <div></div>
     )
