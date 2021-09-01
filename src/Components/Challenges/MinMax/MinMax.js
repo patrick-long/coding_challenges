b8e487a2173eced0022895b4e150a43542d495e3
longpatrick <long.patrick1997@gmail.com>
2021-09-01 17:24:35 -0500

    write display code for functional component MinMax

--- src/Components/Challenges/MinMax/MinMax.js
+++ src/Components/Challenges/MinMax/MinMax.js
@@ -7,7 +7,8 @@ const MinMax = () => {
         result: ''
     });
 
-    const minMaxFunction = (arr) => {
+    const minMaxFunction = (str) => {
+        const arr = str.split('');
         if (arr.length > 0) {
             return [Math.min(...arr), Math.max(...arr)];
         };
@@ -30,8 +31,29 @@ const MinMax = () => {
     };
 
     return (
-        <div></div>
-    )
+        <div className="challenge-container">
+            <h2 className="challenge-title">Find the min and max of numbers array</h2>
+            <p className="challenge-description">Input a series of numbers in an array (i.e. [1, 2, 3])</p>
+            <form 
+                className="challenge-form" 
+                onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={state.input} 
+                    onChange={handleChange} 
+                    name="input" 
+                    className="form-control challenge-input" 
+                    placeholder="Input array here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
+    );
 };
 
 export default MinMax;
