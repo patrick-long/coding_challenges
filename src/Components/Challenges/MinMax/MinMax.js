a8e7d13b1fbde81265bff56958a9f62985929e94
longpatrick <long.patrick1997@gmail.com>
2021-09-01 17:14:47 -0500

    write handleChange and handleSubmit functions

--- src/Components/Challenges/MinMax/MinMax.js
+++ src/Components/Challenges/MinMax/MinMax.js
@@ -2,7 +2,22 @@ import { useState } from 'react';
 
 const MinMax = () => {
 
-
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    };
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = camelCasingFunction(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }));
+    };
+    
     return (
         <div></div>
     )
