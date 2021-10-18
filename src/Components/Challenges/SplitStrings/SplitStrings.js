a3b9d7dd92b09cc31b384e35cd17fa945e06017a
longpatrick <long.patrick1997@gmail.com>
2021-10-18 16:49:12 -0500

    write return statement for SplitStrings component

--- src/Components/Challenges/SplitStrings/SplitStrings.js
+++ src/Components/Challenges/SplitStrings/SplitStrings.js
@@ -9,7 +9,28 @@ const SplitStrings = () => {
 
 
     return (
-        <div></div>
+        <div className="challenge-container">
+            <h2 className="challenge-title">Separate a string every 2 characters!</h2>
+            <p className="challenge-description">Input a series string to separate</p>
+            <form 
+                className="challenge-form" 
+                onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={state.input} 
+                    onChange={handleChange} 
+                    name="input" 
+                    className="form-control challenge-input" 
+                    placeholder="Input string here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
     );
 };
 
