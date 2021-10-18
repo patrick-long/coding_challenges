dbd004930a90780c6cd17d367fd1137d5fc1a123
longpatrick <long.patrick1997@gmail.com>
2021-10-18 16:49:50 -0500

    write handleChange and handleSubmit functions

--- src/Components/Challenges/SplitStrings/SplitStrings.js
+++ src/Components/Challenges/SplitStrings/SplitStrings.js
@@ -7,6 +7,22 @@ const SplitStrings = () => {
         result: ''
     });
 
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    };
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = splitStringsFunction(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }));
+    };
+
 
     return (
         <div className="challenge-container">
