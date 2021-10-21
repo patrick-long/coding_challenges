9a24c01ac1794397541078f4dd811455eb9417f8
longpatrick <long.patrick1997@gmail.com>
2021-10-20 19:29:40 -0500

    write handleChange and handleSubmit functions

--- src/Components/Challenges/SeriesSum/SeriesSum.js
+++ src/Components/Challenges/SeriesSum/SeriesSum.js
@@ -7,6 +7,22 @@ const SeriesSum = () => {
         result: ''
     });
 
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    };
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = seriesSumFunction(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }));
+    };
+
 
     return (
         <div className="challenge-container">
