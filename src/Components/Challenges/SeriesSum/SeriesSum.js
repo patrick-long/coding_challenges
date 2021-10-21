2b448ef3e4daf38b9952193ae9ec115b93927520
longpatrick <long.patrick1997@gmail.com>
2021-10-20 19:06:04 -0500

    write return statement for SeriesSum component

--- src/Components/Challenges/SeriesSum/SeriesSum.js
+++ src/Components/Challenges/SeriesSum/SeriesSum.js
@@ -9,7 +9,28 @@ const SeriesSum = () => {
 
 
     return (
-        <div></div>
+        <div className="challenge-container">
+            <h2 className="challenge-title">Series Sum</h2>
+            <p className="challenge-description">Input a number to find the series sum</p>
+            <form 
+                className="challenge-form" 
+                onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={state.input} 
+                    onChange={handleChange} 
+                    name="input" 
+                    className="form-control challenge-input" 
+                    placeholder="Input number here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
     );
 };
 
