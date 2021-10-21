1370b172ee08820467302efef990313a602b55ef
longpatrick <long.patrick1997@gmail.com>
2021-10-20 19:30:25 -0500

    write seriesSumFunction

--- src/Components/Challenges/SeriesSum/SeriesSum.js
+++ src/Components/Challenges/SeriesSum/SeriesSum.js
@@ -7,6 +7,17 @@ const SeriesSum = () => {
         result: ''
     });
 
+    const seriesSumFunction = n => {
+        let result = 0;
+        let counter = 1;
+        for (let i = 0; i < n; i++) {
+          result += 1/counter;
+          counter += 3;
+        }
+        
+        return result.toFixed(2).toString();
+    }
+
     const handleChange = e => {
         setState(prevState => ({
             ...prevState,
