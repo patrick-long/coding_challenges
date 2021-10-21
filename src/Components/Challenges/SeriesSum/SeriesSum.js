ba1ebb9964b7a6dab4f9c2d5d2485f30a6a5618d
longpatrick <long.patrick1997@gmail.com>
2021-10-20 19:30:43 -0500

    fix indention in seriesSumFunction

--- src/Components/Challenges/SeriesSum/SeriesSum.js
+++ src/Components/Challenges/SeriesSum/SeriesSum.js
@@ -11,8 +11,8 @@ const SeriesSum = () => {
         let result = 0;
         let counter = 1;
         for (let i = 0; i < n; i++) {
-          result += 1/counter;
-          counter += 3;
+            result += 1/counter;
+            counter += 3;
         }
         
         return result.toFixed(2).toString();
