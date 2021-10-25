257087ab310bc71ed183190117ebaf478d3542cb
longpatrick <long.patrick1997@gmail.com>
2021-10-25 10:01:30 -0500

    implement useState react hook in NextSquare component

--- src/Components/Challenges/NextSquare/NextSquare.js
+++ src/Components/Challenges/NextSquare/NextSquare.js
@@ -2,6 +2,11 @@ import { useState } from 'react';
 
 const NextSquare = () => {
 
+    const [state, setState] = useState({
+        input: '',
+        result: ''
+    });
+
     const findNextSquare = sq => {
         if (Math.sqrt(sq) % 1 === 0) {
             return ( Math.sqrt(sq) + 1 ) * ( Math.sqrt(sq) + 1);
