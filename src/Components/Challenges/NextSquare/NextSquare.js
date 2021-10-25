148a6a0457e9e588c0a3df31fb23a3575ded3a8d
longpatrick <long.patrick1997@gmail.com>
2021-10-25 10:03:24 -0500

    write return statement for NextSquare component

--- src/Components/Challenges/NextSquare/NextSquare.js
+++ src/Components/Challenges/NextSquare/NextSquare.js
@@ -17,7 +17,28 @@ const NextSquare = () => {
 
 
     return (
-        <div></div>
+        <div className="challenge-container">
+            <h2 className="challenge-title">Next Perfect Square</h2>
+            <p className="challenge-description">Input a number to find the next perfect square</p>
+            <form 
+                className="challenge-form" 
+                onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={state.input} 
+                    onChange={handleChange} 
+                    name="input" 
+                    className="form-control challenge-input" 
+                    placeholder="Input number here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
     );
 };
 
