b3a47f35cce45c74f06f933a6c2e95d846d9985d
longpatrick <long.patrick1997@gmail.com>
2021-10-26 09:47:17 -0500

    write return statement for SequenceSum component

--- src/Components/Challenges/SequenceSum/SequenceSum.js
+++ src/Components/Challenges/SequenceSum/SequenceSum.js
@@ -9,7 +9,28 @@ const SequenceSum = () => {
 
 
     return (
-        <div></div>
+        <div className="challenge-container">
+            <h2 className="challenge-title">Sequence Sum</h2>
+            <p className="challenge-description">Input a string of numbers to find the sequence sum in "num, num, num" format</p>
+            <form 
+                className="challenge-form" 
+                onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={state.input} 
+                    onChange={handleChange} 
+                    name="input" 
+                    className="form-control challenge-input" 
+                    placeholder="Input numbers here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
     );
 };
 
