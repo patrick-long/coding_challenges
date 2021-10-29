fb905026276705bcfe3d600f418cb1f73054151a
longpatrick <long.patrick1997@gmail.com>
2021-10-29 09:10:56 -0500

    write handleChange and handleSubmit functions

--- src/Components/Challenges/RecursiveCounter/RecursiveCounter.js
+++ src/Components/Challenges/RecursiveCounter/RecursiveCounter.js
@@ -7,6 +7,22 @@ const RecursiveCounter = () => {
         result: ''
     });
 
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    };
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = recursiveCounterFunction(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }));
+    };
+
 
     return (
         <div className="challenge-container">
