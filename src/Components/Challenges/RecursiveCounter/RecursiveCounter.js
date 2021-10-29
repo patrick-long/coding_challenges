dc6688b9349f13beab8b062f51d1e7370af17097
longpatrick <long.patrick1997@gmail.com>
2021-10-29 09:09:16 -0500

    write return statement for RecursiveCounter component

--- src/Components/Challenges/RecursiveCounter/RecursiveCounter.js
+++ src/Components/Challenges/RecursiveCounter/RecursiveCounter.js
@@ -3,8 +3,29 @@ import { useState } from 'react';
 const RecursiveCounter = () => {
 
 
-    return ( 
-        <div></div>
+    return (
+        <div className="challenge-container">
+            <h2 className="challenge-title">Recursive Counter</h2>
+            <p className="challenge-description">Input a number for a recursive function to count to</p>
+            <form 
+                className="challenge-form" 
+                onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={state.input} 
+                    onChange={handleChange} 
+                    name="input" 
+                    className="form-control challenge-input" 
+                    placeholder="Input number here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
     );
 };
 
