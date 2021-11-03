1d68d0a8804f720952ab1ce19eb8be92c6d93d93
longpatrick <long.patrick1997@gmail.com>
2021-11-02 20:17:17 -0500

    write handleChange and handleSubmit functions

--- src/Components/Challenges/LongestWord/LongestWord.js
+++ src/Components/Challenges/LongestWord/LongestWord.js
@@ -15,6 +15,22 @@ const LongestWord = () => {
         };
     };
 
+    const handleChange = e => {
+        setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    };
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        const challengeResult = longestWordFunction(state.input);
+        setState(prevState => ({
+            input: prevState.input,
+            result: challengeResult
+        }));
+    };
+
 
     return (
         <div className="challenge-container">
