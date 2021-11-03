328a610ccb018d050c2e4916f26a05792cf40503
longpatrick <long.patrick1997@gmail.com>
2021-11-02 20:16:48 -0500

    write return statement for LongestWord component

--- src/Components/Challenges/LongestWord/LongestWord.js
+++ src/Components/Challenges/LongestWord/LongestWord.js
@@ -17,7 +17,28 @@ const LongestWord = () => {
 
 
     return (
-        <div></div>
+        <div className="challenge-container">
+            <h2 className="challenge-title">Longest Word</h2>
+            <p className="challenge-description">Input a string to find the length of the longest word</p>
+            <form 
+                className="challenge-form" 
+                onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={state.input} 
+                    onChange={handleChange} 
+                    name="input" 
+                    className="form-control challenge-input" 
+                    placeholder="Input string here"
+                />
+                <button type="submit" className="btn btn-success">
+                    Result
+                </button>
+            </form>
+            <p id="challenge-result">
+                {state.result}
+            </p>
+        </div>
     );
 };
 
