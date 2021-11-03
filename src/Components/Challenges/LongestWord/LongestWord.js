bccfc3649f256306c5bac048e8220104e498d524
longpatrick <long.patrick1997@gmail.com>
2021-11-02 20:09:04 -0500

    implement state and setState functions

--- src/Components/Challenges/LongestWord/LongestWord.js
+++ src/Components/Challenges/LongestWord/LongestWord.js
@@ -2,6 +2,19 @@ import { useState } from 'react';
 
 const LongestWord = () => {
 
+    const [state, setState] = useState({
+        input: '',
+        result: ''
+    });
+
+    const longestWordFunction = str => {
+        const strArray = str.split(' ');
+        let longestCount = 0;
+        for (let i = 0; i < strArray.length; i++) {
+            strArray[i] > longestCount ? longestCount = strArray[i] : longestCount;
+        };
+    };
+
 
     return (
         <div></div>
